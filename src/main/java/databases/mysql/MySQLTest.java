f9f7b84209adc3c63ab628adc3fd311512ca675f
Pavel Syu <poet.poet@gmail.com>
2022-05-25 17:01:53 +0300

    try with resources and errors are human-readable.

--- src/main/java/databases/mysql/MySQLTest.java
+++ src/main/java/databases/mysql/MySQLTest.java
@@ -8,32 +8,20 @@ import java.sql.*;
 public class MySQLTest {
 
 
-    public static void main(String[] args) throws SQLException {
-        Connection conn = null;
-        Statement stmt = null;
-        ResultSet rs = null;
+    public static void main(String[] args) {
 
-        try {
-           // conn = DriverManager.getConnection(CONN_STRING, USERNAME, PASSWORD);
-            conn = DBUtil.getConnection(DBType.MYSQL);
-            stmt = conn.createStatement(ResultSet.TYPE_SCROLL_INSENSITIVE, ResultSet.CONCUR_READ_ONLY);
-            rs = stmt.executeQuery("DESCRIBE pet");
+        try (
+                Connection conn = DBUtil.getConnection(DBType.MYSQL);
+                Statement stmt = conn.createStatement(ResultSet.TYPE_SCROLL_INSENSITIVE, ResultSet.CONCUR_READ_ONLY);
+                ResultSet rs = stmt.executeQuery("DESCRIBE pet");
+
+        ) {
 
             rs.last();
             System.out.println("number of rows " + rs.getRow());
 
         } catch (SQLException e) {
-            e.printStackTrace();
-        } finally {
-            if (rs != null) {
-                rs.close();
-            }
-            if (stmt != null) {
-                stmt.close();
-            }
-            if (conn != null) {
-                conn.close();
-            }
+            DBUtil.processException(e);
         }
     }
 
